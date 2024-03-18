0e1738d11fd7be98d534a66a602fb5c35310dce9
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-03-18 16:52:16 +0100

    triggers updates

---
+++ progetto/database/tables/trigger.sql
@@ -0,0 +1,34 @@
+-- TRIGGER
+
+-- AGGIORNA LA TABELLA CORRISPONDENTE IN BASE AL TIPO DELL'UTENTE CREATO NELLA TABELLA UTENTI
+
+CREATE OR REPLACE FUNCTION aggiorna_tabella()
+RETURNS TRIGGER AS $$
+DECLARE
+    new_matricola INTEGER;
+BEGIN
+    IF NEW.tipo = 'studente' THEN
+        new_matricola := genera_matricola();
+        INSERT INTO universal.studenti (id, matricola)
+        VALUES (NEW.id, new_matricola);
+    ELSIF NEW.tipo = 'docente' THEN
+        INSERT INTO universal.docenti (id, ufficio)
+        VALUES (NEW.id, 'edificio 74');
+    ELSIF NEW.tipo = 'segretario' THEN
+        INSERT INTO universal.segretari (id, sede)
+        VALUES (NEW.id, 'sede centrale');
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- ELIMINA IL DOCENTE, L'EX STUDENTE O IL SEGRETARIO, E L'UTENTE CORRISPONDENTE
+CREATE OR REPLACE FUNCTION elimina_utente_dopo_cancellazione()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM universal.utenti WHERE id = OLD.id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
